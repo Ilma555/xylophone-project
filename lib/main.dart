07a309a43ff81a67a4b020b084186745063a8781
WisteriaFields <145373446+Ilma555@users.noreply.github.com>
2024-11-13 22:57:10 +0800

    commit wav

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
+
+void playSound(int A){
+  final player = AudioPlayer();
+  player.play(AssetSource('note.$A.wav'));
+}
+Expanded keys (int A, Color color){
+  return Expanded(
+    child: TextButton(
+      style: TextButton.styleFrom(
+        backgroundColor: color,
+        shape: const BeveledRectangleBorder(borderRadius: BorderRadius.zero),
+      ),
+      onPressed: (){
+        playSound(A);
+      },
+      child: Container(),
+    ),
+  );
+  
+}
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
@@ -10,7 +31,18 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              keys(1, Colors.red) ,
+              keys(2, Colors.orange),
+              keys(3, Colors.yellow),
+              keys(4, Colors.green),
+              keys(5, Colors.teal),
+              keys(6, Colors.blue),
+              keys(7, Colors.indigo),
+            ],
+
+          ),
         ),
       ),
     );
